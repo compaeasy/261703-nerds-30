3d241efa8283e4194154b9ffbf52e199805e9ad8
compaeasy <compaeasy@yandex.ru>
2021-03-18 20:37:24 +0300

    Готовый проект

---
+++ js/script.js
@@ -0,0 +1,22 @@
+	//Modal
+
+  var modal = document.getElementById("modal-contacts");
+  var	openButton = document.getElementById("open-button");
+  var closeButton = document.getElementById("close-button");
+
+  openButton.onclick = function () {
+    modal.style.display = "block";
+  }
+
+  closeButton.onclick = function () {
+    modal.style.display = "none";
+  }
+  
+
+  const list = document.querySelectorAll('.site-list__item a, .basket')
+  list.forEach(item =>{ 
+         item.addEventListener('click', (e) =>{
+         list.forEach(el=>{ el.classList.remove('_active'); });
+         item.classList.add('_active')
+     })
+  })
